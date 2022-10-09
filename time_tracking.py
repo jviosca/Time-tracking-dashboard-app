9d978745b3a9dfd627ba2c78a650e0646ab8f407
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-09 20:56:54 +0200

    first upload

---
+++ time_tracking.py
@@ -0,0 +1,39 @@
+import streamlit as st
+st.set_page_config(layout="wide", initial_sidebar_state="auto", page_title="Xpeer dashboard", page_icon="chart_with_upwards_trend")
+
+#user authentication. 
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.header('Home')
+    st.write("Welcome to the Xpeer dashboard")
+    st.write("Choose an option in the sidebar")
+    
+
