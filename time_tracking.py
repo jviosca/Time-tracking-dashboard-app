fff1e3d8eb987e2841e9b8d2013ea2d1d41985a8
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-10 13:21:10 +0200

    Update time_tracking.py

--- time_tracking.py
+++ time_tracking.py
@@ -138,7 +138,7 @@ def pie_chart(df):
 	colors = ['#FF0000', '#0000FF', '#FFFF00', '#ADFF2F', '#FFA500']
 	explode = []
 	for i in range(len(x)):
-		explode.append('0.05')
+		explode.append(0.05)
 	st.write(explode)
 	plt.pie(x, colors=colors, labels=df.index, autopct='%1.1f%%', pctdistance=0.85, explode=explode)
 	#plt.pie(x, colors=colors, labels=df.index, autopct='%1.1f%%', pctdistance=0.85)
