ecb6a751a9e46c69e1b583dbabd603d67ea3ec14
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-10 17:42:24 +0200

    Update time_tracking.py

--- time_tracking.py
+++ time_tracking.py
@@ -144,9 +144,10 @@ def pie_chart(df):
 		explode.append(0.05)
 	#plt.pie(x, colors=colors, labels=df.index, autopct='%1.1f%%', pctdistance=0.85, explode=explode)
 	#plt.pie(x1, colors=colors, labels=df.index, autopct="{:.1f}%\n({:d})".format(x1), pctdistance=0.85, explode=explode) 
+	
 	#plt.pie(x1, colors=colors, labels=df.index, autopct=lambda t: datetime.fromtimestamp(t).strftime("%H:%M:%S"), pctdistance=0.85, explode=explode) 
-	st.write(len(df.index.tolist()))
-	st.write(x)
+	st.write(type(df.index.tolist()))
+	st.write(type(x))
 	
 	#plt.pie(x, colors=colors, labels=df.index.tolist(), autopct=lambda t: datetime.fromtimestamp(x[0]/1000.0).strftime("%H:%M:%S"), pctdistance=0.85, explode=explode) 
 	#centre_circle = plt.Circle((0, 0), 0.50, fc='white')
