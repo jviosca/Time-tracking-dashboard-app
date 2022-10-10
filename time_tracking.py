8c711414ed8d0b7499bc66443ea70bf5289fccaf
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-10 17:25:49 +0200

    Update time_tracking.py

--- time_tracking.py
+++ time_tracking.py
@@ -139,7 +139,7 @@ def pie_chart(df):
 	x = df.to_numpy()[0]
 	colors = ['#FF0000', '#0000FF', '#FFFF00', '#ADFF2F', '#FFA500']
 	explode = []
-	for i in range(len(x1)):
+	for i in range(len(x)):
 		explode.append(0.05)
 	#plt.pie(x, colors=colors, labels=df.index, autopct='%1.1f%%', pctdistance=0.85, explode=explode)
 	#plt.pie(x1, colors=colors, labels=df.index, autopct="{:.1f}%\n({:d})".format(x1), pctdistance=0.85, explode=explode) 
