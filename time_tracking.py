0e6b90935e7f8befd5cd7f54301f5219c8f2a324
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-17 16:29:09 +0200

    pdf table styling

--- time_tracking.py
+++ time_tracking.py
@@ -212,8 +212,8 @@ def df2report(df):
     ax.set_axis_off()
     the_table = ax.table(cellText=df.values, rowLabels=df.index, colLabels=df.columns)
     the_table.auto_set_font_size(False)
-    the_table.set_fontsize(14)
-    #st.pyplot(fig)
+    the_table.set_fontsize(10)
+    st.pyplot(fig)
     return fig
     
 
