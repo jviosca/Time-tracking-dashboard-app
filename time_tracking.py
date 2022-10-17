4782ef4e25f80b039fa3c026294b1d5cac1dc75c
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-17 16:19:17 +0200

    pdf table fontsize

--- time_tracking.py
+++ time_tracking.py
@@ -210,7 +210,7 @@ def pie_chart(df):
 def df2report(df):
     fig, ax = plt.subplots()
     ax.set_axis_off()
-    the_table = ax.table(cellText=df.values, rowLabels=df.index, colLabels=df.columns)
+    the_table = ax.table(cellText=df.values, rowLabels=df.index, colLabels=df.columns, fontsize=12.0)
     #st.pyplot(fig)
     return fig
     
