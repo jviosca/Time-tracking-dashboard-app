c103943ee306cc1fdfaf643469c2aa3317b7576f
jviosca <105012646+jviosca@users.noreply.github.com>
2022-10-17 18:16:40 +0200

    pdf table columns

--- time_tracking.py
+++ time_tracking.py
@@ -478,19 +478,27 @@ if check_password():
             line_height = pdf.font_size * 2.5
             epw = pdf.w - pdf.l_margin
             col_width = epw / 7  # distribute content evenly
-            x_col = 1
+            x_col = 0
             top = pdf.y
-            for rowIndex, row in df.iterrows(): #iterate over rows
-                
+            for column_name,column_content in df.items():
                 offset = pdf.x + (x_col * col_width)
-                pdf.y = top
-                for columnIndex, value in row.items():
-                    
-                    pdf.x = offset
-                    #pdf.multi_cell(col_width, line_height, datum, border=1, new_x="RIGHT", new_y="TOP", max_line_height=pdf.font_size)
-                    pdf.multi_cell(col_width, line_height, value, border=1)
+                pdf.y = top                
+                pdf.x = offset
+                pdf.multi_cell(col_width, line_height, column_content[1], border=1)
                 pdf.ln(line_height)
                 x_col = x_col + 1
+                pdf.ln(line_height)
+                x_col = x_col + 1
+                                                
+            #for rowIndex, row in df.iterrows(): #iterate over rows
+             #   offset = pdf.x + (x_col * col_width)
+              #  pdf.y = top
+               # for columnIndex, value in row.items():
+                #    pdf.x = offset
+                    #pdf.multi_cell(col_width, line_height, datum, border=1, new_x="RIGHT", new_y="TOP", max_line_height=pdf.font_size)
+                 #   pdf.multi_cell(col_width, line_height, value, border=1)
+                #pdf.ln(line_height)
+                #x_col = x_col + 1
         html = create_download_link(pdf.output(dest="S").encode("latin-1"), "report")
         st.markdown(html, unsafe_allow_html=True)
         
